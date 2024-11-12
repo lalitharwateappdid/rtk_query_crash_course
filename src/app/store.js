8a6654d4fbc018de67d09af1edd9b48cc07b2058
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-12 12:12:25 +0530

    updated fetching completed

---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { postsApi } from "../features/posts/postsApi";
+
+export const store = configureStore({
+    reducer:{
+        [postsApi.reducerPath] : postsApi.reducer
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(postsApi.middleware),
+})
---
+++ src/features/posts/postsApi.jsx
@@ -0,0 +1,18 @@
+// src/features/posts/postsApi.js
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const postsApi = createApi({
+  reducerPath: 'postsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => 'posts',
+    }),
+    getPost: builder.query({
+      query: (id) => `posts/${id}`,
+    }),
+  }),
+});
+
+// Export the hooks that will be generated
+export const { useGetPostsQuery, useGetPostQuery } = postsApi;
