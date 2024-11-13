bf037cd32684dfa842a3fdd9239db167f60b7489
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-13 10:17:41 +0530

    loader added to the react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 // src/App.js
-import React from 'react';
-import { useGetPostsQuery } from './features/posts/postsApi';
+import React from "react";
+import { useGetPostsQuery } from "./features/posts/postsApi";
+
+// assets
+import loader from "./assets/loader.webm";
 
 function App() {
   // Using the RTK Query hook to fetch posts
   const { data: posts, error, isLoading } = useGetPostsQuery();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading)
+    return (
+      <div style={{display:"flex", justifyContent:"center", width:"100%"}}>
+        
+        <video width="320" height="240" autoplay loop muted>
+          <source src={loader} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+       
+      </div>
+    );
   if (error) return <div>Error loading posts!</div>;
 
   return (
